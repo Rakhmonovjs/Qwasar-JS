d7a55f176bb78b45b6508e6e12646e1dfb992b3c
Student astrum <student@astrum.domain>
2022-06-25 10:45:52 +0500

    qwest04

---
+++ qwest04/ex02/my_first_if_multiple_conditions.js
@@ -0,0 +1,13 @@
+a = 10
+b = 9
+c = 11
+d = 10
+y = 9
+z = 11
+
+if (a) {
+  console.log("a is bigger than b AND smaller than c AND equal to d");
+}
+if (z) {
+  console.log("z OR y are bigger than a");
+}
