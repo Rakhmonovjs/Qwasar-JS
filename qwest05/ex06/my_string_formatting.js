86a87a135c004da4045f54c72d1f8a2f6a71b4bf
Student astrum <student@astrum.domain>
2022-06-25 10:55:31 +0500

    qwest05

---
+++ qwest05/ex06/my_string_formatting.js
@@ -0,0 +1,8 @@
+function my_string_formatting(param_1, param_2, param_3) {
+    // param_1 = firstname;
+    // param_2 = lastname;
+    // param_3 = age;
+    
+    console.log("Hello, my name is " + param_1  +" " + param_2 + ", I'm "+ param_3 +".");
+};
+// "Hello, my name is FIRSTNAME LASTNAME, I'm AGE."
